d5ea177edb2597b9da1c8ec49ae866322c26544a
Aleguid97 <aleguid97@gmail.com>
2024-01-12 14:45:08 +0100

    PrimoCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Navbar, Row, Col } from 'react-bootstrap';
+import CitySearch from './components/CitySearch';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const handleCitySelect = async (city) => {
+    const API_KEY = 'f07e23e3c9bbfe73897c8d5ff15322cd';
+    const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+    try {
+      const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+      const data = await response.json();
+
+      const cityData = {
+        name: data.name,
+        sys: data.sys,
+        weather: data.weather,
+        main: data.main,
+      };
+
+      setSelectedCity(cityData);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      setSelectedCity(null);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <Navbar bg="light" expand="lg" className="mb-4">
+        <h1 className='ms-3'>App Meteo</h1>
+      </Navbar>
+      <Row className="">
+        <Col md={6}>
+          <CitySearch onCitySelect={handleCitySelect} />
+        </Col>
+      </Row>
+      {selectedCity && (
+        <Row className="justify-content-center mt-4">
+        </Row>
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/components/CitySearch.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Form, Button, Card, ListGroup } from 'react-bootstrap';
+
+const CitySearch = ({ onCitySelect }) => {
+  const [city, setCity] = useState('');
+  const [citiesWithSameName, setCitiesWithSameName] = useState([]);
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSelectCity = async () => {
+    if (city.trim() !== '') {
+      const API_KEY = 'f07e23e3c9bbfe73897c8d5ff15322cd';
+      const BASE_URL = 'https://api.openweathermap.org/data/2.5/find';
+
+      try {
+        const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        const data = await response.json();
+
+
+        const citiesWithSameName = data.list.filter((cityData) => cityData.name.toLowerCase() === city.toLowerCase());
+
+        setCitiesWithSameName(citiesWithSameName);
+      } catch (error) {
+        console.error('Error fetching city data:', error);
+        setCitiesWithSameName([]);
+      }
+
+      onCitySelect(city);
+    }
+  };
+
+  return (
+    <div>
+      <Form className="d-flex mb-4">
+        <Form.Group controlId="formCity" className="mr-2">
+          <Form.Control
+            type="text"
+            placeholder="Enter city"
+            value={city}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Button variant="primary ms-3" onClick={handleSelectCity} className="mx-auto">
+          Get Weather
+        </Button>
+      </Form>
+      
+      {citiesWithSameName.length > 0 && (
+        <div>
+          <h4>Città:</h4>
+          <div className='d-inline-flex'>
+            {citiesWithSameName.map((cityData, index) => (
+              <Card key={index} className="m-2 shadow-lg" style={{ width: '10em' }}>
+                <Card.Body>
+                  <Card.Title>{cityData.name}, {cityData.sys.country}</Card.Title>
+                  <Card.Img
+                    variant="top"
+                    src={`http://openweathermap.org/img/wn/${cityData.weather[0].icon}.png`}
+                    alt="Weather Icon"
+                  />
+                  <ListGroup>
+                    <ListGroup.Item>Temperature: {cityData.main.temp}°C</ListGroup.Item>
+                    <ListGroup.Item>Min Temperature: {cityData.main.temp_min}°C</ListGroup.Item>
+                    <ListGroup.Item>Max Temperature: {cityData.main.temp_max}°C</ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CitySearch;
